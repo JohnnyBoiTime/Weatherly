6626ae10df4054f6cec6b8170bf669e8fa047000
Philip Rickey <philip.rickey@wsu.edu>
2023-06-22 22:08:56 -0700

    commit

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
+import * as React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+
+// Main app
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer> 
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={homeScreen} />   
+        <Stack.Screen name="details" component={detailsScreen} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+const Stack = createNativeStackNavigator();
+
+// Home
+function homeScreen() {
+  return (
+    <View style={styles.container}>
+          <Text>Home </Text>
+        </View>
+  );
+}
+
+// details
+function detailsScreen() {
+  return (
+    <View style={styles.container}>
+          <Text>details</Text>
+        </View>
+  );
+}
+
+
+
+// Style for text
 const styles = StyleSheet.create({
   container: {
     flex: 1,
